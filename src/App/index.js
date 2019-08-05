b7049e78ac4255250c7f6904603ae592a83287b1
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-05 16:46:31 -0400

    Add AppBar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import styled, { css } from "styled-components";
 import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 
 function App() {
   return (
     <div>
-      <AppLayout>Welcome </AppLayout>
+      <AppLayout>
+        <AppBar />
+        <h1>Welcome to Crypto Dashboard</h1>{" "}
+      </AppLayout>
     </div>
   );
 }
