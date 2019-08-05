65b49de296a66e240543ab15f2ada17b8ae4291b
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-05 16:37:05 -0400

    Add App Layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,12 @@
 import React from "react";
 import "./App.css";
 import styled, { css } from "styled-components";
-
-const Button = styled.a`
-  /* This renders the buttons above... Edit me! */
-  display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  background: transparent;
-  color: white;
-  border: 2px solid white;
-
-  ${props =>
-    props.primary &&
-    css`
-      background: white;
-      color: palevioletred;
-    `}
-`;
+import AppLayout from "./AppLayout";
 
 function App() {
   return (
     <div>
-      <Button primary>GitHub</Button>
+      <AppLayout>Welcome </AppLayout>
     </div>
   );
 }
