d17e0d457e08c414f0b3a39af6f14513f9cf6370
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-05 16:30:08 -0400

    Add Styled Components

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./App.css";
+import styled, { css } from "styled-components";
+
+const Button = styled.a`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: white;
+      color: palevioletred;
+    `}
+`;
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Button primary>GitHub</Button>
+    </div>
+  );
 }
 
 export default App;
